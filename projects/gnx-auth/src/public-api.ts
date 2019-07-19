62683f0e29dd5c8d1d5175126762cff862f5e4ed
Gleb Ilinykh <gleb.ilinykh@gmail.com>
2019-07-19 17:59:21 +0300

    chore: Add dist

--- projects/gnx-auth/src/public-api.ts
+++ projects/gnx-auth/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './lib/gnx-auth.service';
 export * from './lib/guards/allow-non-logged-user.guard';
 export * from './lib/guards/require-logged-user.guard';
 export * from './lib/gnx-auth.module';
+
+export {GnxAuthModule} from './lib/gnx-auth.module';
